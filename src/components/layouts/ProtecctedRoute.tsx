ca7b593fa788793b5e8ad044b7db253319c95816
devhasanmia <dev.hasanmia@gmail.com>
2025-08-25 18:24:41 +0600

    remove login

--- src/components/layouts/ProtecctedRoute.tsx
+++ src/components/layouts/ProtecctedRoute.tsx
@@ -1,45 +1,28 @@
 import { Navigate } from "react-router";
 import type { ReactNode } from "react";
-import { useLogoutMutation, useProfileQuery } from "../../redux/features/auth/authApi";
-import { useEffect } from "react";
+import { useProfileQuery } from "../../redux/features/auth/authApi";
 
 type TProtectedRoute = {
-    children: ReactNode;
-    role?: string;
+  children: ReactNode;
+  role?: string;
 };
 
 const ProtectedRoute = ({ children, role }: TProtectedRoute) => {
-    const { data: user, isLoading } = useProfileQuery(undefined, {
-        refetchOnMountOrArgChange: true,
-    });
+  const { data: user, isLoading } = useProfileQuery(undefined, {
+    refetchOnMountOrArgChange: true,
+  });
 
-    const [logout] = useLogoutMutation();
+  if (isLoading) return null;
 
-    useEffect(() => {
-        const handleLogout = async () => {
-            try {
-                await logout({}).unwrap();
-            } catch (err) {
-                console.error("Logout failed", err);
-            }
-        };
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
 
-        if (user?.data?.isBlocked) {
-            handleLogout();
-        }
-    }, [user?.data?.isBlocked]);
+  if (role && user?.data?.role !== role) {
+    return <Navigate to="/access-denied" replace />;
+  }
 
-    if (isLoading) return null;
-
-    if (!user) {
-        return <Navigate to="/login" replace />;
-    }
-
-    if (role && user?.data?.role !== role) {
-        return <Navigate to="/access-denied" replace />;
-    }
-
-    return <>{children}</>;
+  return <>{children}</>;
 };
 
 export default ProtectedRoute;
